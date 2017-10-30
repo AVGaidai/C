f3dcb1b5e22fdfa65b7800408c717ae59c1d9736
AVGaidai <diligent20494@gmail.com>
2017-10-30 21:21:48 +0700

    deleted unnecessary comments; add init_colors()

--- file_editor_ncurses/main.c
+++ file_editor_ncurses/main.c
@@ -5,52 +5,62 @@
 
 /**
  * \brief Function screen initialization.
- *xs
+ * 
+ * To get character-at-a-time input without echoing
+ * (most interactive, screen oriented programs want
+ * this), the following sequence should be used:
+ *   initscr(); cbreak(); noecho();
+ * Most programs would additionally use the sequence:
+ *   nonl();
+ *   intrflush(stdscr, FALSE);
+ *   keypad(stdscr, TRUE);
+ *
  * \return the integer 1 upon failure and 0 upon successful completion.
  */
 int init_scr(void)
 {
-   /*
-    * The initscr code determines the terminal type
-    * and initializes all curses data structures 
-    */
     if (initscr() == NULL)
         return 1;
-   /*
-    * The cbreak routine disables line buffering and 
-    * erase/kill character-processing (interrupt and
-    * flow control characters are unaffected), making
-    * characters typed by the user immediately avail‐
-    * able to the program
-    */ 
+  
     if (cbreak() == ERR)
         return 1;
-   /*
-    * The echo and noecho routines control whether
-    * characters typed by the user are echoed by
-    * getch() as they are typed
-    */
+  
     if (noecho() == ERR)
         return 1;
-   /*
-    * The nl and nonl routines control whether the 
-    * underlying display device translates the return
-    * key into newline on input, and whether it tran-
-    * slates newline into return and line-feed on out‐
-    * put. Initially, these translations do occur. If
-    * you disable them using nonl, curses will be able
-    * to make better use of the line-feed capability,
-    * resulting in faster cursor motion.
-    */
+  
     nonl();
+
     if (intrflush(stdscr, FALSE) == ERR)
         return 1;
+
     if (keypad(stdscr, TRUE) == ERR)
         return 1;
 
     return 0;
 }
     
+
+int init_colors(void)
+{
+    if (!has_colors())
+        return 1;
+    
+    curs_set(0); /* Invisible cursor */
+
+    if (start_color() == ERR)
+        return 1;
+    
+    if (init_pair(1, COLOR_WHITE, COLOR_BLUE) == ERR)
+        return 1;
+    
+    if (init_pair(2, COLOR_WHITE, COLOR_CYAN) == ERR)
+        return 1;
+    
+    if (init_pair(3, COLOR_WHITE, COLOR_GREEN) == ERR)
+        return 1;
+
+    return 0;
+}
 
 
 int main(int argc, char *argv[])
@@ -59,27 +69,23 @@ int main(int argc, char *argv[])
     PANEL *panels[3], *top;
     int i, ch;
     
-    init_stdscr()
-
-
-    if (!has_colors()) {
-        endwin();
-        printf("Error: not support colors!\n");
+    if (init_scr()) {
+        fprintf(stderr, "Error into init_scr()...\n");
         return 1;
     }
 
-    curs_set(0);
-    start_color();
-    init_pair(1, COLOR_RED, COLOR_BLUE);
-    init_pair(2, COLOR_GREEN, COLOR_RED);
-    init_pair(3, COLOR_BLUE, COLOR_GREEN);
+    if (init_colors()) {
+        fprintf(stderr, "Error: not support colors!\n");
+        endwin();
+        return 1;
+    }
     
     for (i = 0; i < 3; ++i) {
         windows[i] = newwin(20 + i * 2, 100 - i * 10, 2 + i, i * 10);
         wbkgdset(windows[i], COLOR_PAIR(i + 1));
         wclear(windows[i]);
-        box(windows[i], ACS_VLINE, ACS_HLINE);
         mvwprintw(windows[i], 1, 1, "hello world!!! I'am panel %d\n", i);
+        box(windows[i], ACS_VLINE, ACS_HLINE);
         wrefresh(windows[i]);
 
         panels[i] = new_panel(windows[i]);
