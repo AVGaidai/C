8ffae8748f0cbe1eab3841a9d800852a98803085
AVGaidai <diligent20494@gmail.com>
2017-12-27 00:43:47 +0700

    fixed program

--- Simulation Modeling/task_01/main.c
+++ Simulation Modeling/task_01/main.c
@@ -17,24 +17,25 @@ struct task {
 
     double arrival_time;
     int id;
-
+    int transition;
+    
 };
 
 
 struct event {
 
     double time;
-    int device_num;
-    int task_num;
-
+    int device_id;
+    int task_id;
+    
 };
 
 
 struct device {
 
     double time;
+    double last_time;
     int id;
-    int completed_tasks;
 
     struct task *task;
     struct queue queue;
@@ -51,16 +52,19 @@ int compare(const void *a, const void *b)
     d1 = *((struct device *) a);
     d2 = *((struct device *) b);
 
-    t1 = d1.completed_tasks * d1.time + d1.time;
+    t1 = d1.last_time;
     if (d1.task != NULL) {
-        t1 += d1.task->arrival_time;
+        t1 = (d1.task->arrival_time > t1) ? d1.task->arrival_time : t1;
     }
+    t1 += d1.time;
+    
 
-    t2 = d2.completed_tasks * d2.time + d2.time;
+    t2 = d2.last_time;
     if (d2.task != NULL) {
-        t2 += d2.task->arrival_time;
+        t2 = (d2.task->arrival_time > t2) ? d2.task->arrival_time : t2;
     }
-
+    t2 += d2.time;
+    
     return (t1 > t2) ? 1 : 0;
 }
 
@@ -75,10 +79,13 @@ void schedule_create(struct queue *schedule,
         
     for (i = 0; i < k; ++i) {
         if (device[i].task != NULL) {
-            
-            device[i].task->arrival_time +=
-                device[i].time * (device[i].completed_tasks++) + device[i].time;
 
+            if (device[i].last_time < device[i].task->arrival_time) {
+                device[i].last_time = device[i].task->arrival_time;
+            }
+            device[i].task->arrival_time = device[i].last_time + device[i].time;
+            device[i].last_time = device[i].task->arrival_time;
+            
             for (j = 0; device[j].id != (device[i].id + 1) % k; ++j);
             queue_push(&(device[j].queue), device[i].task, sizeof(struct task));
 
@@ -93,8 +100,8 @@ void schedule_create(struct queue *schedule,
                 free(device[i].task);
                 continue;
             }
-            event.device_num = device[i].id;
-            event.task_num = device[i].task->id;
+            event.device_id = device[i].id;
+            event.task_id = device[i].task->id;
             queue_push(schedule, &event, sizeof(struct event));
 
             free(device[i].task);
@@ -108,19 +115,41 @@ void schedule_create(struct queue *schedule,
 }
 
 
+
+struct event *get_event(struct queue *schedule)
+{
+    size_t size;
+    
+    return (struct event *) queue_pop(schedule, &size);
+}
+
+
+void execute_event(struct event *event, struct task *task)
+{
+    ++task[event->task_id].transition;
+}
+
+
+void remove_event(struct event *event)
+{
+    free(event);
+}
+
+
 int main(int argc, char *argv[])
 {
-    int k = 5, N = 10, T = 1000;
-    int i, opt;
+    int k = 5, N = 10;
+    int i, j, r, opt, events_num;
+    int *distr, *state;
     size_t size;
-    double x;
-    
+    double x, T = 1000.00;
+        
     struct event *event;
     struct queue schedule;
     struct device *device;
     struct task *task;
-    
-        
+
+            
     while ((opt = getopt(argc, argv, "k:N:T:")) != -1) {
         switch (opt) {
         case 'k':
@@ -130,13 +159,18 @@ int main(int argc, char *argv[])
             N = atoi(optarg);
             break;
         case 'T':
-            T = atoi(optarg);
+            T = atof(optarg);
             break;
         default:
             ;
         }
     }
-    printf("k=%d\nN=%d\nT=%d\n", k, N, T);
+
+    if (T <= 0.0 || N <= 0 || k <= 0) {
+        return 1;
+    }
+    
+    printf("k=%d\nN=%d\nT=%.2f\n", k, N, T);
 
     device = (struct device *) malloc(k * sizeof(struct device));
     
@@ -145,40 +179,70 @@ int main(int argc, char *argv[])
     for (i = 0; i < k; ++i) {
         x = rand() / (double) RAND_MAX;
         device[i].time = -1.00 * log(1.00 - x) / (i + 1);
+        device[i].last_time = 0.00;
         device[i].id = i;
         queue_init(&(device[i].queue));
     }
-    device[0].time = 1.98;
-    device[1].time = 0.66;
-    device[2].time = 0.13;
-    device[3].time = 0.11;
-    device[4].time = 0.08;
     
     qsort(device, k, sizeof(struct device), compare);
 
     task = (struct task *) malloc(N * sizeof(struct task));
-    
+    state = (int *) calloc(k, k * sizeof(int));
     for (i = 0; i < N; ++i) {
         task[i].arrival_time = 0.00;
         task[i].id = i;
-        printf("%d\n", i % k);
-        queue_push(&(device[i % k].queue), &task[i], sizeof(struct task));
+        task[i].transition = 0;
+        r = rand() % k;
+        for (j = 0; device[j].id != r; ++j);            
+        queue_push(&(device[j].queue), &task[i], sizeof(struct task));
+        ++state[device[j].id];
     }
 
     for (i = 0; i < k; ++i) {
-        printf("device #%d time: %f\n", device[i].id, device[i].time);
+        printf("device #%d time: %f | start task number %d\n",
+               device[i].id, device[i].time, state[device[i].id]);
         device[i].task = (struct task *) queue_pop(&(device[i].queue), &size);
     }
 
     queue_init(&schedule);
     schedule_create(&schedule, device, k, T);
 
-    while ((event = (struct event *) queue_pop(&schedule, &size)) != NULL) {
-        printf("Event (time=%f): task %d from device #%d to device #%d\n",
-               event->time, event->task_num,
-               event->device_num, (event->device_num + 1) % k);
+    distr = (int *) calloc(k, k * sizeof(int));
+    events_num = 0;
+    while ((event = get_event(&schedule)) != NULL) {
+        ++events_num;
+        --state[event->device_id];
+        ++state[(event->device_id + 1) % k];
+        for (i = 0; i < k; ++i) {
+            distr[i] += state[i];
+        }
+        printf("Event (time=%f): task %d from device #%d to device #%d"
+               " | %d tasks on device %d\n",
+               event->time, event->task_id,
+               event->device_id, (event->device_id + 1) % k,
+               state[event->device_id], event->device_id);
+        execute_event(event, task);
+        remove_event(event);
     }
     queue_free(&schedule);
+    free(state);
+
+    printf("distr: ");
+    for (i = 0; i < k; ++i) {
+        printf("%f ", ((distr[i] / (double) events_num) * 100.0) / (double) N);
+    }
+
+    free(distr);
+    printf("\n\n=====================\n");
+    for (i = 0; i < N; ++i) {
+        printf("task #%d\n"
+               "_____________________________\n"
+               "\tcycle number: %d\n"
+               "\tavg cycle time: %f\n\n",
+               i, task[i].transition / k, T / (task[i].transition / k));
+    }
+    printf("=====================\n");    
+    
     free(task);
     
     for (i = 0; i < k; ++i) {
